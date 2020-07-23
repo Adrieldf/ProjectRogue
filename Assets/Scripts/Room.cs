c8ef3222ff164f34c6953f24c55a5448b85b49d7
Adrieldf <adriel.idf@gmail.com>
2020-07-23 01:25:15 -0300

    start of procedural rooms

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<Transform> StartPositions;
+    public List<Transform> EndPositions;
+    public RoomType RoomType;
+
+    [SerializeField]
+    private List<Transform> _enemySpawnerPositions;
+
+    private void Awake()
+    {
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.gameObject.name);
+        }
+    }
+
+    private void Start()
+    {
+        SpawnEnemies();
+    }
+
+
+    void SpawnEnemies()
+    {
+        foreach (Transform pos in _enemySpawnerPositions)
+        {
+            //pegar de uma pool de inimigos e spawnar randomicamente algum dentro dela
+        }
+    }
+}
+
+public enum RoomType
+{
+    Basic,
+    Dangerous,
+    Treasure,
+    Trap,
+
+}
+
---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public List<GameObject> Rooms;
+
+    private void Start()
+    {
+        
+    }
+}
